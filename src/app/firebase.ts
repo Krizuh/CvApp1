44e2a64d0024a73de61568e469d945ea6602cf26
Krizuh <krizuh.h@gmail.com>
2019-05-02 18:14:00 +0300

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyCru6d99LDvYhOOf9Yksi9t99_PJv1M5Fo",
+    authDomain: "cvschool-fadee.firebaseapp.com",
+    databaseURL: "https://cvschool-fadee.firebaseio.com",
+    projectId: "cvschool-fadee",
+    storageBucket: "cvschool-fadee.appspot.com",
+    messagingSenderId: "1077366025399"
+  };
+
+export default config
